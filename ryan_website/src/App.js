1494cea37c5acf251e1bc530696fafbba0c6be93
mosiman <dillonchan97@gmail.com>
2018-10-31 19:13:37 -0400

    nice transition on AboutMe load, have minimally working React-Router

--- ryan_website/src/App.js
+++ ryan_website/src/App.js
@@ -4,32 +4,46 @@ import './style.css';
 
 // TODO: more elegant / maintainable static file serving
 import bredtoes from './images/Bred Toes.jpg';
+import ryanpic from './images/ryanpic.jpg';
+
+// SPA router (react-router)
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 // importing fontawesome icons
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCamera, faUser } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-
 library.add(fab, faCamera, faUser);
 
-const instaLinkStyle = {
+
+
+const linkStyle = {
     color: "white",
     textDecorationLine: "none"
 };
 
+class AboutMe extends Component{
 
-class RCBackground extends Component{
+    render(){
+
+        return (
+            <div id="rc-about">
+                <img src="images/ryanpic.jpg"  alt=""/>
+            </div>
+        );
+    };
+    
 
 
+}
+
+class Homepage extends Component{
     render() {
         return (
-            <div id="rc-bg"
+            <div id="rc-homepage"
                 style = {{ backgroundImage: bredtoes }} >
-                <NavFloat />
-
             </div>
-
         );
 
     };
@@ -42,12 +56,12 @@ class NavFloat extends Component {
     render() {
         return (
             <div id="rc-logo">
-                Ryan Chan
+                <Link to="/" style={linkStyle}> Ryan Chan </Link>
                 <div id="navlinks">
                     <ul>
                         <FontAwesomeIcon icon="user" />
                         <span className="navtext">
-                            {'  '} about me
+                            {'  '} <Link to="/about" style={linkStyle}> about me </Link>
                         </span>
                     </ul>
                     <ul>
@@ -59,11 +73,10 @@ class NavFloat extends Component {
                     <ul>
                         <FontAwesomeIcon icon={['fab', 'instagram']} />
                         <span className="navtext">
-                            <a href="http://google.ca" style={instaLinkStyle}> {'  '}bowlglet</a>
+                            <a href="http://google.ca" style={linkStyle}> {'  '}bowlglet</a>
                         </span>
                     </ul>
-
-                </div>
+</div>
             </div>
 
         );
@@ -95,7 +108,14 @@ class NavFloat extends Component {
 class App extends Component {
   render() {
     return (
-        <RCBackground />
+        <Router>
+            <div>
+            <NavFloat />
+
+            <Route exact path="/" component={Homepage} />
+            <Route path="/about" component={AboutMe} />
+            </div>
+        </Router>
     );
   }
 }
